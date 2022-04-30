9a309537b48728157382b879a9caedf041f6f1ef
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-04-30 13:09:24 +0200

    Draw more complex objects

--- OpenRA.Game/Graphics/PolygonRenderable.cs
+++ OpenRA.Game/Graphics/PolygonRenderable.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenRA.Primitives;
+using SharpGLTF.Schema2;
 
 namespace OpenRA.Graphics
 {
@@ -13,6 +14,7 @@ namespace OpenRA.Graphics
 		readonly WVec offset;
 		readonly int zOffset;
 		readonly bool isDecoration;
+		Vertex[] verticesArray;
 
 		public PolygonRenderable(WPos pos, WVec offset, int zOffset, bool isDecoration)
 		{
@@ -20,6 +22,50 @@ namespace OpenRA.Graphics
 			this.offset = offset;
 			this.zOffset = zOffset;
 			this.isDecoration = isDecoration;
+
+
+			var test = ModelRoot.Load(@"C:\Dev\other\openra\cubes.glb");
+
+			var verticesList = new List<Vertex>();
+			foreach (var cube in test.DefaultScene.VisualChildren)
+			{
+
+				var primitive = cube.Mesh.Primitives.First();
+				var transform = cube.LocalTransform.Matrix;
+				var matrix = new float[] {
+				transform.M11,
+				transform.M12,
+				transform.M13,
+				transform.M14,
+				transform.M21,
+				transform.M22,
+				transform.M23,
+				transform.M24,
+				transform.M31,
+				transform.M32,
+				transform.M33,
+				transform.M34,
+				transform.M41,
+				transform.M42,
+				transform.M43,
+				transform.M44,
+			};
+
+				var indices = primitive.IndexAccessor.AsIndicesArray();
+				var vertices = primitive.GetVertexAccessor("POSITION").AsVector3Array();
+
+				for (var i = 0; i < indices.Count; i++)
+				{
+					var index = (int)indices[i];
+					var vertex = vertices[index];
+					var vertexFloatArray = new float[] { vertex.X, vertex.Y, vertex.Z, 1 };
+
+					vertexFloatArray = Util.MatrixVectorMultiply(matrix, vertexFloatArray);
+
+					verticesList.Add(new Vertex(new float3(555 + vertexFloatArray[0] * 10, 525 + vertexFloatArray[1] * 10, vertexFloatArray[2] * 10), 1, 0, 0, 1, 0, 0));
+				}
+			}
+			verticesArray = verticesList.ToArray();
 		}
 
 		public WPos Pos => pos + offset;
@@ -47,7 +93,7 @@ namespace OpenRA.Graphics
 			var tr = new float3(br.X, tl.Y, tl.Z);
 			var bl = new float3(tl.X, br.Y, br.Z);
 			var points = new[] { tl, tr, br, bl };
-			var vertices = new Vertex[6];
+
 			// Not a line
 			if (points.Length < 2)
 				return;
@@ -79,14 +125,16 @@ namespace OpenRA.Graphics
 			var cd = end - corner;
 
 			var Offset = new float3(0.5f, 0.5f, 0f);
-			// Fill segment
-			vertices[0] = new Vertex(ca + Offset, r, g, b, a, 0, 0);
-			vertices[1] = new Vertex(cb + Offset, r, g, b, a, 0, 0);
-			vertices[2] = new Vertex(cc + Offset, r, g, b, a, 0, 0);
-			vertices[3] = new Vertex(cc + Offset, r, g, b, a, 0, 0);
-			vertices[4] = new Vertex(cd + Offset, r, g, b, a, 0, 0);
-			vertices[5] = new Vertex(ca + Offset, r, g, b, a, 0, 0);
-			Game.Renderer.SpriteRenderer.DrawRGBAVertices(vertices, blendMode);
+
+
+
+			//vertices[0] = new Vertex(ca + Offset, r, g, b, a, 0, 0);
+			//vertices[1] = new Vertex(cb + Offset, r, g, b, a, 0, 0);
+			//vertices[2] = new Vertex(cc + Offset, r, g, b, a, 0, 0);
+			//vertices[3] = new Vertex(cc + Offset, r, g, b, a, 0, 0);
+			//vertices[4] = new Vertex(cd + Offset, r, g, b, a, 0, 0);
+			//vertices[5] = new Vertex(ca + Offset, r, g, b, a, 0, 0);
+			Game.Renderer.SpriteRenderer.DrawRGBAVertices(verticesArray, blendMode);
 		}
 
 		public void RenderDebugGeometry(WorldRenderer wr)
