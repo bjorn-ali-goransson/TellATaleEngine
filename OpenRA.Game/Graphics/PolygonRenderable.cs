bf009a5c897e2da588cf2b5c548d469d6e711ee6
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-05-01 22:51:56 +0200

    Cleanup

--- OpenRA.Game/Graphics/PolygonRenderable.cs
+++ OpenRA.Game/Graphics/PolygonRenderable.cs
@@ -33,23 +33,23 @@ namespace OpenRA.Graphics
 				var primitive = cube.Mesh.Primitives.First();
 				var transform = cube.LocalTransform.Matrix;
 				var matrix = new float[] {
-				transform.M11,
-				transform.M12,
-				transform.M13,
-				transform.M14,
-				transform.M21,
-				transform.M22,
-				transform.M23,
-				transform.M24,
-				transform.M31,
-				transform.M32,
-				transform.M33,
-				transform.M34,
-				transform.M41,
-				transform.M42,
-				transform.M43,
-				transform.M44,
-			};
+					transform.M11,
+					transform.M12,
+					transform.M13,
+					transform.M14,
+					transform.M21,
+					transform.M22,
+					transform.M23,
+					transform.M24,
+					transform.M31,
+					transform.M32,
+					transform.M33,
+					transform.M34,
+					transform.M41,
+					transform.M42,
+					transform.M43,
+					transform.M44,
+				};
 
 				var indices = primitive.IndexAccessor.AsIndicesArray();
 				var vertices = primitive.GetVertexAccessor("POSITION").AsVector3Array();
@@ -80,61 +80,10 @@ namespace OpenRA.Graphics
 
 		public void Render(WorldRenderer wr)
 		{
-
-
-			var color = Color.Red;
-			var closed = true;
-
-			var width = 1f;
-			var blendMode = BlendMode.Alpha;
 			var p = wr.Screen3DPxPosition(pos);
 			var tl = (float3)wr.Viewport.WorldToViewPx(p);
-			var br = (float3)wr.Viewport.WorldToViewPx(p + new float3(10, 10, 10));
-			var tr = new float3(br.X, tl.Y, tl.Z);
-			var bl = new float3(tl.X, br.Y, br.Z);
-			var points = new[] { tl, tr, br, bl };
 
-			// Not a line
-			if (points.Length < 2)
-				return;
-
-			// Single segment
-			if (points.Length == 2)
-			{
-				Game.Renderer.RgbaColorRenderer.DrawLine(points[0], points[1], width, color, blendMode);
-				return;
-			}
-
-			color = Util.PremultiplyAlpha(color);
-			var r = color.R / 255.0f;
-			var g = color.G / 255.0f;
-			var b = color.B / 255.0f;
-			var a = color.A / 255.0f;
-
-			var start = points[0];
-			var end = points[1];
-			var dir = (end - start) / (end - start).XY.Length;
-			var corner = width / 2 * new float3(-dir.Y, dir.X, dir.Z);
-
-			// Corners for start of line segment
-			var ca = start - corner;
-			var cb = start + corner;
-
-			// Vertices for the corners joining start-end to end-next
-			var cc = end + corner;
-			var cd = end - corner;
-
-			var Offset = new float3(0.5f, 0.5f, 0f);
-
-
-
-			//vertices[0] = new Vertex(ca + Offset, r, g, b, a, 0, 0);
-			//vertices[1] = new Vertex(cb + Offset, r, g, b, a, 0, 0);
-			//vertices[2] = new Vertex(cc + Offset, r, g, b, a, 0, 0);
-			//vertices[3] = new Vertex(cc + Offset, r, g, b, a, 0, 0);
-			//vertices[4] = new Vertex(cd + Offset, r, g, b, a, 0, 0);
-			//vertices[5] = new Vertex(ca + Offset, r, g, b, a, 0, 0);
-			Game.Renderer.SpriteRenderer.DrawRGBAVertices(verticesArray, blendMode);
+			Game.Renderer.SpriteRenderer.DrawRGBAVertices(verticesArray, BlendMode.Alpha);
 		}
 
 		public void RenderDebugGeometry(WorldRenderer wr)
